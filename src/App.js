11667c43c1c99bb0e2b9e87bcfc8444802d9e0a9
Vaibhav <gupta.vaibhav2990@gmail.com>
2024-01-15 23:22:29 +0530

    fixed login logic required for accesss to dashboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ element, isLoggedIn }) => {
+  return isLoggedIn ? element : <Navigate to="/" />;
+};
 
 function App() {
   const [isSignedUp, setIsSignedUp] = useState(false);
@@ -18,16 +22,20 @@ function App() {
 
   return (
     <Router>
-      {isLoggedIn ? (
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Login onLogin={handleLogin} />} />
-          <Route path="/signup" element={<SignUp onSignUp={handleSignUp} />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route
+          path="/"
+          element={<Login onLogin={handleLogin} />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUp onSignUp={handleSignUp} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} isLoggedIn={isLoggedIn} />}
+        />
+      </Routes>
     </Router>
   );
 }
